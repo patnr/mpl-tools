cd2962a13772d688ab3f555e5e8fd76ef343869d
patricknraanes <patrick.n.raanes@gmail.com>
2020-12-08 17:47:22 +0100

    Fix warning missing dist_name

--- src/mpl_tools/__init__.py
+++ src/mpl_tools/__init__.py
@@ -7,16 +7,16 @@ from mpl_tools.ax_placement import *
 from mpl_tools.log_toggler import *
 from mpl_tools.misc import *
 
+# Change here if project is renamed and does not equal the package name
+dist_name = 'mpl-tools'
+
+if mpl.get_backend()=="Qt5Agg":
+    print("Warning: Qt5Agg is used, "
+    "which has been known to cause slow-down with " + dist_name)
+
 try:
-    # Change here if project is renamed and does not equal the package name
-    dist_name = 'mpl-tools'
     __version__ = get_distribution(dist_name).version
 except DistributionNotFound:
     __version__ = 'unknown'
 finally:
     del get_distribution, DistributionNotFound, dist_name
-
-
-if mpl.get_backend()=="Qt5Agg":
-    print("Warning: Qt5Agg is used, "
-    "which has been known to cause slow-down with "+dist_name)
