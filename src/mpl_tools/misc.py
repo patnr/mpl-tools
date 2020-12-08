b3655b3cd01f8c64af9b9bba0c30712e6829d50e
patricknraanes <patrick.n.raanes@gmail.com>
2020-12-08 17:48:49 +0100

    Autopep8. Split fig_placement_save (and load) in smaller funcs.

--- src/mpl_tools/misc.py
+++ src/mpl_tools/misc.py
@@ -1,29 +1,34 @@
 import json
+import os
+import platform
 
 import matplotlib as mpl
 from matplotlib import pyplot as plt
 
-__all__ = ["thousands", "freshfig",
-           "fig_placement_save", "fig_placement_load",
-           "get_legend_bbox", "xFontsize",
-           "is_notebook_or_qt", "axprops", "fig_colorbar"]
+__all__ = [
+    "thousands", "freshfig",
+    "get_fig_geo", "set_fig_geo", "fig_placement_save", "fig_placement_load",
+    "get_legend_bbox", "xFontsize",
+    "is_notebook_or_qt", "axprops", "fig_colorbar"]
 
 thousands = mpl.ticker.StrMethodFormatter('{x:,.7g}')
+
+_FIG_GEOMETRIES_PATH = "./.fig_geometries"
 
 try:
     __IPYTHON__
     from IPython import get_ipython
     is_notebook_or_qt = 'zmq' in str(type(get_ipython())).lower()
-except (NameError,ImportError):
+except (NameError, ImportError):
     is_notebook_or_qt = False
 
 
-def freshfig(num=None,figsize=None,*args,**kwargs):
+def freshfig(num=None, figsize=None, *args, **kwargs):
     """Create/clear figure.
 
-    Similar to::
+    Similar to:
 
-      fig, ax = suplots(*args,**kwargs)
+    >>> fig, ax = suplots(*args,**kwargs)
 
     With the modification that:
 
@@ -32,52 +37,72 @@ def freshfig(num=None,figsize=None,*args,**kwargs):
     - Otherwise: clear figure.
       Avoids closing/opening so as to keep pos and size.
     """
-    exists = plt.fignum_exists(num)
+    # exists = plt.fignum_exists(num)
 
-    fig = plt.figure(num=num,figsize=figsize)
+    fig = plt.figure(num=num, figsize=figsize)
     fig.clear()
 
-    _, ax = plt.subplots(num=fig.number,*args,**kwargs)
+    _, ax = plt.subplots(num=fig.number, *args, **kwargs)
     return fig, ax
 
 
-
-def get_fmw(fignum):
+def _get_fmw(fignum):
     """If this fails, there's probs no way to make placement work."""
     return plt.figure(fignum).canvas.manager.window
 
-def fig_placement_save(path="./.fig_geometries"):
-    placements = {}
-    for fignum in plt.get_fignums():
-        fmw = get_fmw(fignum)
-        try: # For Qt4Agg/Qt5Agg
-            geo = dict(
-                w = fmw.width(),
-                h = fmw.height(),
-                x = fmw.x(),
-                y = fmw.y(),
-            )
-        except: # For TkAgg
-            geo = fmw.geometry()
-        placements[fignum] = geo
-    with open(path,"w") as F:
+
+def get_fig_geo(fignum):
+    fmw = _get_fmw(fignum)
+    try:
+        # For Qt4Agg/Qt5Agg
+        return dict(
+            w=fmw.width(),
+            h=fmw.height(),
+            x=fmw.x(),
+            y=fmw.y(),
+        )
+    except Exception:
+        # For TkAgg
+        return fmw.geometry()
+
+
+def set_fig_geo(fignum, geo):
+    plt.figure(int(fignum))
+    fmw = _get_fmw(int(fignum))
+    try:
+        # For Qt4Agg/Qt5Agg
+        fmw.setGeometry(geo['x'], geo['y'], geo['w'], geo['h'])
+    except Exception:
+        # For TkAgg
+        # geo = "{w:.0f}x{h:.0f}+{x:.0f}+{y:.0f}".format(**geo)
+        fmw.geometry(newGeometry=geo)
+
+
+def fig_placement_save(path=_FIG_GEOMETRIES_PATH, append_host=True):
+    if append_host:
+        path = ".".join([path, platform.node()])
+
+    placements = {num: get_fig_geo(num) for num in plt.get_fignums()}
+
+    with open(path, "w") as F:
         F.write(json.dumps(placements))
 
-def fig_placement_load(path="./.fig_geometries"):
-    with open(path,"r") as F:
+
+def fig_placement_load(path=_FIG_GEOMETRIES_PATH, append_host=True):
+    if append_host:
+        path = ".".join([path, platform.node()])
+
+    if not os.path.exists(path):
+        print("Warning: no saved figure placement found"
+              " under the name " + path +
+              ". Create a new one using fig_placement_save().")
+        return
+
+    with open(path, "r") as F:
         placements = json.load(F)
-    for fignum in placements:
-        plt.figure(int(fignum))
-        fmw = get_fmw(int(fignum))
-        geo = placements[fignum]
-        try: # For Qt4Agg/Qt5Agg
-            fmw.setGeometry( geo['x'], geo['y'], geo['w'], geo['h'])
-        except: # For TkAgg
-            # geo = "{w:.0f}x{h:.0f}+{x:.0f}+{y:.0f}".format(**geo)
-            fmw.geometry(newGeometry=geo)
 
-
-
+    for num in placements:
+        set_fig_geo(num, placements[num])
 
 
 def get_legend_bbox(ax):
@@ -92,14 +117,11 @@ def get_legend_bbox(ax):
     return inner
 
 
-def xFontsize(fontsize,fig,*args):
+def xFontsize(fontsize, fig, *args):
     """Multiply by fontsize, in pixels (rather than points)."""
     plt.pause(.1)
     fontsize = fig.canvas.renderer.points_to_pixels(fontsize)
     return tuple(a*fontsize for a in args)
-
-
-
 
 
 # stackoverflow.com/a/11103301
@@ -111,31 +133,32 @@ def on_xlim_changed(ax):
     for ax in fig.axes:
         ax.callbacks.connect('xlim_changed', on_xlim_changed)
     """
+    import numpy as np
     xlim = ax.get_xlim()
     for a in ax.figure.axes:
-            # shortcuts: last avoids n**2 behavior when each axis fires event
-            if a is ax or len(a.lines) == 0 or getattr(a, 'xlim', None) == xlim:
-                    continue
+        # shortcuts: last avoids n**2 behavior when each axis fires event
+        if a is ax or len(a.lines) == 0 or getattr(a, 'xlim', None) == xlim:
+            continue
 
-            ylim = np.inf, -np.inf
-            for l in a.lines:
-                    x, y = l.get_data()
-                    # faster, but assumes that x is sorted
-                    start, stop = np.searchsorted(x, xlim)
-                    yc = y[max(start-1,0):(stop+1)]
-                    ylim = min(ylim[0], np.nanmin(yc)), max(ylim[1], np.nanmax(yc))
+        ylim = np.inf, -np.inf
+        for ln in a.lines:
+            x, y = ln.get_data()
+            # faster, but assumes that x is sorted
+            start, stop = np.searchsorted(x, xlim)
+            yc = y[max(start-1, 0):(stop+1)]
+            ylim = min(ylim[0], np.nanmin(yc)), max(ylim[1], np.nanmax(yc))
 
-            # TODO: update limits from Patches, Texts, Collections, ...
+        # TODO: update limits from Patches, Texts, Collections, ...
 
-            # x axis: emit=False avoids infinite loop
-            a.set_xlim(xlim, emit=False)
+        # x axis: emit=False avoids infinite loop
+        a.set_xlim(xlim, emit=False)
 
-            # y axis: set dataLim, make sure that autoscale in 'y' is on
-            corners = (xlim[0], ylim[0]), (xlim[1], ylim[1])
-            a.dataLim.update_from_data_xy(corners, ignore=True, updatex=False)
-            a.autoscale(enable=True, axis='y')
-            # cache xlim to mark 'a' as treated
-            a.xlim = xlim
+        # y axis: set dataLim, make sure that autoscale in 'y' is on
+        corners = (xlim[0], ylim[0]), (xlim[1], ylim[1])
+        a.dataLim.update_from_data_xy(corners, ignore=True, updatex=False)
+        a.autoscale(enable=True, axis='y')
+        # cache xlim to mark 'a' as treated
+        a.xlim = xlim
 
 
 def axprops(dct):
@@ -156,11 +179,12 @@ def axprops(dct):
             props.append(ax+p)
 
     # intersection(dct,props)
-    props = {p:dct.pop(p) for p in props if p in dct}
+    props = {p: dct.pop(p) for p in props if p in dct}
 
     return props
 
-def fig_colorbar(fig,collections,*args,**kwargs):
+
+def fig_colorbar(fig, collections, *args, **kwargs):
     """Add colorbar to the right on a figure."""
     fig.subplots_adjust(right=0.8)
     cax = fig.add_axes([0.85, 0.15, 0.05, 0.7])
