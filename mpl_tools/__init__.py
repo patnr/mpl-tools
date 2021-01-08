4b84c0a0d3dc7261209e0e80b15b29920a0c4ab7
patricknraanes <patrick.n.raanes@gmail.com>
2021-01-08 11:03:34 +0100

    Rm module imports in __init__

--- mpl_tools/__init__.py
+++ mpl_tools/__init__.py
@@ -1,12 +1,6 @@
 from importlib.metadata import version
 from pathlib import Path
 
-import matplotlib as mpl
-
-from mpl_tools.ax_placement import *
-from mpl_tools.log_toggler import *
-from mpl_tools.misc import *
-
 try:
     __version__ = version(__name__)
 except:
