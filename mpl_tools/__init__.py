e24ede0de4ef08f9382462e0fb25857c61b1d4fc
patricknraanes <patrick.n.raanes@gmail.com>
2021-01-13 15:31:27 +0100

    Add comments

--- mpl_tools/__init__.py
+++ mpl_tools/__init__.py
@@ -5,14 +5,25 @@ Want tests to run on any level (pytest, tox, travis)
 Screw cookiecutter and pyscaffold
 coc-settings
 Poetry/pyproject
-Installation
-- pip
-- make
+Linting
+ - Editor: useful
+ - Precommit: useful
+ - Travis: for PRs
+ - As part of pytest - maybe for heavier (eg pylint) linting.
+ - As part of tox - nah. This is one overlap too many
+
+Installation and tasks with Makefile
+
 Pytest
 - Using mpl image comparison
 Tox
 pyenv
 Travis-CI
+
+    # AFAIK, the main reason to use tox on travis-ci (as well as locally)
+    # is just to avoid repeating ourselves.
+
+
 Deploy when git tag is pushed
 """
 
